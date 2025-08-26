6fde504578396542d2a6ca01748d0b537c25178d
Kalp Mehta <kalpm1832@gmail.com>
2025-08-26 11:33:21 +0530

    Bug fix.

--- qrly/src/lib/supabase.js
+++ qrly/src/lib/supabase.js
@@ -1,13 +1,14 @@
+
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client for browser (client-side)
 export const supabaseBrowser = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "https://xocwhhqlebylgutnnlmx.supabase.co",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvY3doaHFsZWJ5bGd1dG5ubG14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MDYyNTYsImV4cCI6MjA3MTE4MjI1Nn0.25Gv6pYFzMZSWOXk3yGeV_sadZndoSaynQ0a-d5C5p8"
+    process.env.NEXT_PUBLIC_SUPABASE_URL ,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY 
 );
 
 // Initialize Supabase client for server (server-side)
 export const supabaseServer = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "https://xocwhhqlebylgutnnlmx.supabase.co",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvY3doaHFsZWJ5bGd1dG5ubG14Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTYwNjI1NiwiZXhwIjoyMDcxMTgyMjU2fQ.IDGUNDVAAgBFw7v5VKI3M9LhIsuKMwuJj-OrxZxqMuM"
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY 
 );
